1b8f4fe50ba880e5bc9eb8b891b6cf66bbcdf9a6
H.D. Sarson <hds28@tw705.eng.cam.ac.uk>
2016-10-26 14:57:41 +0100

    written part of a routine to sweep frequency and damping to find optimal absorber damping

---
+++ sweepLambda.m
@@ -0,0 +1,58 @@
+
+
+% Requires mPrime, kPrime, lambdaPrime,
+%          absorberMass, absorberStiffness
+%          lambdaRange, omegaRange           to be defined
+
+M = [
+  mPrime, 0;
+  0, absorberMass;
+];
+
+K = [
+  kPrime + absorberStiffness, -absorberStiffness;
+  -absorberStiffness, absorberStiffness;
+];
+
+maxAmpl = [
+  0, NaN;
+  0, NaN;
+];
+
+Ampl = zeros(size(lambdaRange), size(omegaRange), 2);
+
+for i = size(lambdaRange) do
+
+  absorberDamping = lambdaRange(i);
+
+  Lambda = [ 
+    lambdaPrime + absorberDamping, -absorberDamping;
+    -absorberDamping, absorberDamping;
+  ];
+
+  for j =  size(omegaRange) do
+   
+    w = omegaRange(i);
+
+    X = inv(-w^2 * M + 1i * w * Lambda + K) * F;
+    
+    for k = 1:2 do
+
+      Ampl(i, j, k) = X(k);
+
+      if X(k) > maxAmpl(1,k)
+        maxAmpl(:, k) = [X(k), w];
+      
+    end
+
+  end
+end
+
+
+surf(lambdaRange, omegaRange, Ampl(:,:,1));
+title('floor');
+figure;
+surf(lambdaRange, omegaRange, Ampl(:,:,2));
+title('absorber');
+
+maxAmpl
